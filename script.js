b4a7d4d7fad75f98e9fc7663c62e427cb77ce24f
femiclassic11-ai <femiclassic11@gmail.com>
2025-10-02 00:08:42 +0100

    top

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log("wsg Top")
+function ability(){
+
+    //Varables
+    const studentName = ("April");
+    const assignment1 = (90);
+    const assignment2 = (92);
+    const finalExam = (93);
+
+    const assignmentAverage = (91);
+
+    const finalGrade = (finalExam * 0.6) + (assignmentAverage * 0.4);
+
+    const hasPassed = (92.9 >= 70);
+    const isExcellent = (finalGrade >= 90);
+
+    //Output
+    console.log("Student Name: " + studentName);
+    console.log("Assignment 1: " + assignment1);
+    console.log("Assignment 2: " + assignment2);
+    console.log("Final Exam: " + finalExam);
+    console.log("Assignment Average: " + assignmentAverage);
+    console.log("Final Grade: " + finalGrade);
+    console.log("Has Passed: " + hasPassed);
+    console.log("Excellent performance: " + isExcellent);
+
+
+    
+
+
+
+
+    
+}
