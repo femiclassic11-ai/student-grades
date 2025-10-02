6e6b83db62b2ee871f0ef332426a5984d7c47b17
femiclassic11-ai <femiclassic11@gmail.com>
2025-10-02 17:36:24 +0100

    update

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-console.log("April Grades");
+console.log("Student Grade");
 function ability(){
 
     //Varables
     const studentName = ("April");
-    const assignment1 = (90);
-    const assignment2 = (92);
-    const finalExam = (93);
+    const assignment1 = (85);
+    const assignment2 = (72);
+    const finalExam = (91);
 
-    const assignmentAverage = (91);
+    const assignmentAverage = (assignment1 + assignment2) / 2;
 
     const finalGrade = (finalExam * 0.6) + (assignmentAverage * 0.4);
 
-    const hasPassed = (92.9 >= 70);
-    const isExcellent = (finalGrade >= 90);
+    const hasPassed = (finalGrade >= 70);
+    const failed = (finalGrade < 70);
+    const isExcellent = (hasPassed && finalExam >= 90);
+    
+    
 
-    //Output
-    console.log("Student Name: " + studentName);
+
+    //console.log results
+    console.log("Name :" + studentName)
     console.log("Final Grade: " + finalGrade);
     console.log("Has Passed: " + hasPassed);
     console.log("Excellent performance: " + isExcellent);
-
-
-    
-
-
-
-
-    
+    console.log("Failed: " + failed); 
 }
